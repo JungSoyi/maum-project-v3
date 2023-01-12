58b23aa25f6c347f230ca3a5a1850ae6a961a815
JungSoyi <soi0125@naver.com>
2023-01-12 12:18:19 +0900

    :card_file_box: survey-question relation 설정

--- src/survey/entities/survey.entity.ts
+++ src/survey/entities/survey.entity.ts
@@ -1,25 +1,31 @@
 import { ObjectType, Field, Int } from '@nestjs/graphql';
 import { Question } from 'src/question/entities/question.entity';
+import { CreateDateColumn, Entity, OneToMany, UpdateDateColumn } from 'typeorm';
 /**
  * survey_id : number
  * created_at : LocalDateTime
  * updated_at : LocalDateTime
  * total_score : number
  */
-@ObjectType()
-export class Survey {
-  @Field(() => Int, { description: 'survey id' })
-  survey_id: number;
+@ObjectType({ implements: Node })
+@Entity()
+export class Survey implements Node {
+  @Field(() => String)
+  id: string;
 
-  @Field(() => Date)
-  created_at: Date;
-
-  @Field(() => Date)
-  updated_at: Date;
+  @Field(() => Int)
+  survey_number: number;
 
   @Field(() => Int)
   total_score: number;
 
-  @Field(() => [Question])
+  @CreateDateColumn()
+  readonly createdAt: Date;
+
+  @UpdateDateColumn()
+  readonly updatedAt: Date;
+
+  @Field(() => [Question], { nullable: true })
+  @OneToMany(() => Question, (question) => question.survey, { eager: true })
   questions: Question[];
 }
