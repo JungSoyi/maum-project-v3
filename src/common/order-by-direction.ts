33842718d1076fd6d983e30bdded9323b809cb73
JungSoyi <soi0125@naver.com>
2023-01-24 23:16:25 +0900

    :fire: 코드 제거

--- src/common/order-by-direction.ts
+++
@@ -1,8 +0,0 @@
-import { registerEnumType } from "@nestjs/graphql";
-
-export enum OrderByDirection {
-    ASC = 'ASC',
-    DESC = 'DESC',
-}
-
-registerEnumType(OrderByDirection, { name: 'OrderByDirection' });
