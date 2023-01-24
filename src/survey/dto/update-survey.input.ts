1ef5d1a7eec8775c1625662de9518193edeb1f87
JungSoyi <soi0125@naver.com>
2023-01-24 15:47:04 +0900

    :recycle: survey update dto 수정

--- src/survey/dto/update-survey.input.ts
+++ src/survey/dto/update-survey.input.ts
@@ -2,6 +2,10 @@ import { InputType, Field, Int, PartialType } from '@nestjs/graphql';
 
 @InputType()
 export class UpdateSurveyInput {
+
+  @Field(() => String, { nullable: true })
+  title?: string;
+
   @Field(() => Int, { nullable: true })
   survey_number?: number;
 }
